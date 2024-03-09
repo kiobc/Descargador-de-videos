2063b5099c30ad9318d44c8599fe287c092b3846
kiobc <brayan.calderon3464@utc.edu.ec>
2024-03-08 23:13:43 -0500

    Agregada nueva libreria

--- app.py
+++ app.py
@@ -1,35 +1,58 @@
 from flask import Flask, render_template, request, send_file
 import os
-import pafy
-import moviepy.editor as mp
+import subprocess  
 
 app = Flask(__name__)
 path = os.getcwd() + '/output/'
+
+if not os.path.exists(path):
+    os.makedirs(path)
 
 @app.route('/')
 def route():
     return render_template('index.html')
 
-@app.route('/envia', methods=['GET', 'POST'])
+@app.route('/envia', methods=['POST'])
 def envia():
-    if request.method == 'POST':
-        url = request.form['url']
-        video = pafy.new(url)
-        best = video.getbest(prefertype='mp4')
-        p = path + video.title + '.mp4'
-        return send_file(p, as_attachment=True)
+    try:
+        if request.method == 'POST':
+            url = request.form['url']
+            output = path + 'video.mp4'
+            subprocess.run(["yt-dlp", "-o", output, url])
+            return send_file(output, as_attachment=True)
+    except Exception as e:
+        return f"Error: {str(e)}"
 
-@app.route('/envia2', methods=['GET', 'POST'])
+@app.route('/envia2', methods=['POST'])
 def envia2():
-    if request.method == 'POST':
-        url = request.form['url']
-        video = pafy.new(url)
-        best = video.getbest(prefertype='mp3')
-        name = path + video.title + '.mp3'
-        clip = mp.VideoFileClip(name)
-        clip.audio.write_audiofile(path + video.title + ".mp3")
-        p = path + video.title + '.mp3'
-        return send_file(p, as_attachment=True)
+    try:
+        if request.method == 'POST':
+            url = request.form['url']
+            output_webm = path + '%(title)s.%(ext)s'
+            subprocess.run([
+                "yt-dlp",
+                "-x",
+                "--audio-format", "webm",
+                "--ffmpeg-location", r"C:\webm\bin",
+                "-o", output_webm,
+                url
+            ])
+            for file in os.listdir(path):
+                if file.endswith('.webm'):
+                    input_webm = os.path.join(path, file)
+                    output_mp3 = os.path.splitext(input_webm)[0] + '.mp3'
+                    subprocess.run([
+                        r"C:\webm\bin\ffmpeg",
+                        "-i", input_webm,
+                        "-vn",
+                        "-ab", "128k",
+                        "-ar", "44100",
+                        "-y", output_mp3
+                    ])
+                    return send_file(output_mp3, as_attachment=True)
+    except Exception as e:
+        return f"Error: {str(e)}"
+    return "Ocurrió un error durante la descarga o la conversión del archivo."
 
 if __name__ == '__main__':
     app.run(host='localhost')
